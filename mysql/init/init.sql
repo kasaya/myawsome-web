f437266e6cf0a711c367d5f6c89a56752d43fcf3
CANGOONLINE\ouyangchenling <ouyangchenling@cangoonline.com>
2022-01-21 10:36:57 +0800

    update mysql config

--- mysql/init/init.sql
+++ mysql/init/init.sql
@@ -1,8 +1,8 @@
-create database test-db;
-use test-db;
+create database test;
+use test;
 CREATE TABLE `login` (
   `id` int NOT NULL AUTO_INCREMENT,
   `login_cd` varchar(32) DEFAULT NULL,
   `hashed_password` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
